a727d8ecaea88cd62ead91762c5173d00cf618ca
jsanthos01 <chinu.s2001@gmail.com>
2020-02-17 21:16:42 -0500

    created button on second page

--- growth.js
+++ growth.js
@@ -8,23 +8,9 @@ function growthApi(){
       url:"http://harvesthelper.herokuapp.com/api/v1/plants?api_key=324ce6305a7559e91581645b9ae72c7c",
       method: "GET"
    }).then(function (response){
-      //goes through the response object
       for (var i = 0; i < 45; i++){
-         //goes through farmers crop section
             for(var j = 0; j <farmersCropArr.length; j++){
                if(response[i].name == farmersCropArr[j]){
-
-                  //console.log needs to be changed to whatever container that u are putting the information in -----> .append()
-                  console.log(response[i].name);
-                  console.log(response[i].description);
-                  console.log(response[i].optimal_sun);
-                  console.log(response[i].optimal_soil);
-                  console.log(response[i].planting_considerations);
-                  console.log(response[i].when_to_plant);
-                  console.log(response[i].spacing);
-                  console.log(response[i].disease);
-                  console.log(response[i].watering);
-
                   var displayDiv = document.getElementById('plantInfo'); 
                   displayDiv.innerHTML += 
                   `
--- veggie2.js
+++ veggie2.js
@@ -387,8 +387,7 @@ function showSateliteImage(response) {
   let arrayLength = response.length-1
   
   if (response.length===0){
-    // $('#satImage').attr( 'src', '' );
-    alert('No satelite image available!');
+    alert('No satellite image available!');
   } else {
     $('#satImage').attr( 'src', response[arrayLength].image.truecolor);
   }
@@ -406,14 +405,9 @@ function showUvIndex(response) {
 
 function plantsToGrow() {   
     var farmersCrop = [];
-        farmersCrop.length = 0;
-        document.getElementById('veggies').innerHTML="";
-    console.log('running plantsToGrow')
-    // var temp = prompt("Temperature of your Area");
-    // var humidity = prompt('Humidity');
-    // var soilMoisture = prompt('Soil Moisture in your location');
+    farmersCrop.length = 0;
+    document.getElementById('veggies').innerHTML="";    
     
-
     var plantType = "";
     for ( var i = 0; i < veggiesArr.length ; i++) {
         console.log('i have entered first for loop');
@@ -467,7 +461,6 @@ function plantsToGrow() {
                     <p class="veggies-text"><strong>Plant Type:</strong> ${veggiesArr[j].type}</p>
                     <p class="veggies-text"><strong>Days to Grow:</strong> ${veggiesArr[j].timeToGrow} Days</p>
                     <p class="veggies-text"><strong>No of Gallons Per Week:</strong> ${waterForPlant}</p>
-                    <a href="growth.html" class="btn btn-outline-success mx-auto guide-button" role="button" aria-pressed="true" >Learn More</a>
 
                 </div> 
               `
